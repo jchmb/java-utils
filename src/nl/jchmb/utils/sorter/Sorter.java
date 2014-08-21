3e4f55256e4aca0eb52d4dbdc5562b63c5dd7c92
jchmb <barelds.jochem@gmail.com>
2014-08-21 19:31:04 +0200

    Commit

--- src/nl/jchmb/utils/sorter/Sorter.java
+++ src/nl/jchmb/utils/sorter/Sorter.java
@@ -3,6 +3,14 @@ package nl.jchmb.utils.sorter;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Sorter class.
+ * 
+ * @author jchmb
+ *
+ * @param <T>
+ * @param <U>
+ */
 public interface Sorter<T extends Comparable<T>, U extends List<T>> {
 	public void sort(List<T> list);
 	public void sort(List<T> list, Comparator<T> comparator);
