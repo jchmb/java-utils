2c3c6d087aa911ca4ee3e45b6461f786158d91dd
jchmb <barelds.jochem@gmail.com>
2014-08-23 16:45:06 +0200

    Commit

--- src/nl/jchmb/utils/alarm/DefaultAlarmManager.java
+++ src/nl/jchmb/utils/alarm/DefaultAlarmManager.java
@@ -3,7 +3,7 @@ package nl.jchmb.utils.alarm;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public abstract class DefaultAlarmManager implements AlarmManager {
+public class DefaultAlarmManager implements AlarmManager {
 	private Collection<Alarm> alarms;
 	
 	public DefaultAlarmManager() {
